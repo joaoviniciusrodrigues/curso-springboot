f5105da4baef6c5c45afde304677abc7590b0b24
João Vinicius Rodrigues do Nascimento <joaoviniciusrodrigues@users.noreply.github.com>
2020-03-23 17:00:05 -0300

    Order, Instat, ISO 8601 - PART1

--- src/main/java/com/cursojava/curso/entities/Order.java
+++ src/main/java/com/cursojava/curso/entities/Order.java
@@ -1,14 +1,14 @@
 package com.cursojava.curso.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Instant;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -16,35 +16,24 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 @Entity
-@Table(name = "tb_user")
-public class User implements Serializable {
-
+@Table(name = "tb_order")
+public class Order implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private String name;
-	private String email;
-	private String phone;
-	private String password;
+	private Instant moment;
 
-	@OneToMany(mappedBy = "client")
-	private List<Order> orders = new ArrayList<>();
-
-	public User(Long id, String name, String email, String phone, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
-	}
+	@ManyToOne
+	@JoinColumn(name = "client_id")
+	private User client;
 
 	@Override
 	public int hashCode() {
@@ -62,7 +51,7 @@ public class User implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		User other = (User) obj;
+		Order other = (Order) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
