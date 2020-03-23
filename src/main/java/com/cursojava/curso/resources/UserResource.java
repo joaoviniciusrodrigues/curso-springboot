66ab908d63a636b266dd37d367509b5bdf8cecb0
João Vinicius Rodrigues do Nascimento <joaoviniciusrodrigues@users.noreply.github.com>
2020-03-23 15:25:44 -0300

    Service layer, component registration

--- src/main/java/com/cursojava/curso/resources/UserResource.java
+++ src/main/java/com/cursojava/curso/resources/UserResource.java
@@ -1,23 +1,34 @@
 package com.cursojava.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.curso.entities.User;
+import com.cursojava.curso.services.UserService;
 
-@RestController 
-@RequestMapping("/user")
+@RestController
+@RequestMapping("/users")
 public class UserResource {
-	
+
+	@Autowired
+	private UserService userService;
+
 	@GetMapping
-	public ResponseEntity<User>findAll(){
-		User u = new User(1L,"João","joaovinicius@gmail.com","99999999","123456");		
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 
-	
-	
-	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> find(@PathVariable Long id) {
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
+
 }
